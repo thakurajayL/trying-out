dec908b6aa128c31fd440e340ddc7b4caf585658
Ajay Lotan Thakur <thakur.ajay@gmail.com>
2020-04-15 22:09:23 +0000

    Adding few shell examples

---
+++ Shell/hello.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash 
+#this is how we comment in scripts 
+echo Hello World
+echo "One more hello "
---
+++ Shell/variable.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+FIRST_NAME=Ajay #no space before or after the = sign 
+FAV_COLOR=Red #Name should start from letter or _ but not number
+LAST_NAME="Lotan Thakur" # Quote is required to get all words 
+echo Hi $FIRST_NAME $LAST_NAME, your favorite color is $FAV_COLOR
