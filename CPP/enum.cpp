089c63b7f5b871c93f2451f3e60c343fdd747ded
Ajay Lotan Thakur <thakur.ajay@gmail.com>
2020-04-26 17:19:21 +0000

    Enum test program for Cpp

---
+++ CPP/enum.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main()
+{
+	enum days {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY};
+	days F = MONDAY;
+	std::cout<<"\n u have entered "<<F<<std::endl;
+	return 0;
+}
